90be1a318b9557ae116fe94d79c3fca87b002145
Rfaze07 <69684348+Rfaze07@users.noreply.github.com>
2025-05-30 10:57:32 -0300

    Se cambio el menu lateral por un menu superior

--- js/script.js
+++ js/script.js
@@ -1,28 +1,19 @@
 function mostrarSeccion(id) {
-    const secciones = document.querySelectorAll('.seccion');
-    secciones.forEach(seccion => {
-        seccion.style.display = 'none';
-    });
+  const secciones = document.querySelectorAll('.seccion');
+  secciones.forEach(seccion => {
+    seccion.style.display = 'none';
+  });
 
-    const activa = document.getElementById(id);
-    if (activa) {
-        activa.style.display = 'block';
-    }
+  const activa = document.getElementById(id);
+  if (activa) {
+    activa.style.display = 'block';
+  }
 }
 
-const botonMenu = document.getElementById("toggle-menu");
-const menuLateral = document.querySelector(".menu-lateral");
 
-botonMenu.addEventListener("click", () => {
-    menuLateral.classList.toggle("abierto");
-});
+const toggleBtn = document.getElementById("toggle-menu");
+const menu = document.querySelector(".navbar-menu");
 
-const botonesMenu = document.querySelectorAll(".menu-lateral button");
-
-botonesMenu.forEach(boton => {
-    boton.addEventListener("click", () => {
-        if (window.innerWidth <= 768) {
-            menuLateral.classList.remove("abierto");
-        }
-    });
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("abierto");
 });
